f942bcbb8fbaeb22197e9860af911fb1e2846803
Alex Makarov <makarovav@answer-42.ru>
2026-08-03 06:59:01 +0700

    fix notification model

--- internal/domain/notification.go
+++ internal/domain/notification.go
@@ -3,12 +3,13 @@ package domain
 import "time"
 
 type Notification struct {
-	ID          int    `json:"id"`
+	ID int `json:"id"`
+
 	ProjectPath string `json:"project_path"`
 	MRIID       int    `json:"mr_iid"`
+	Status      string `json:"status"` // new | done
 
-	Text   string `json:"text"`
-	Status string `json:"status"` // new | done
+	Text string `json:"text"`
 
 	ReplyToMessageId string `json:"reply_to_message_id"`
 
