57d2fce147536611d502b2fcb55db9bf5f1a4ab0
Alex Makarov <makarovav@answer-42.ru>
2026-08-05 14:37:04 +0700

    remove old

--- internal/redis/keys.go
+++
@@ -1,7 +0,0 @@
-package redis
-
-import "fmt"
-
-func MRKey(projectPath string, mrID int) string {
-	return fmt.Sprintf("mr:%s:%d", projectPath, mrID)
-}
--- internal/utils/makeKey.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "fmt"
-
-func MakeKey(projectPath string, mrId int) string {
-	return fmt.Sprintf("%s:%d", projectPath, mrId)
-}
