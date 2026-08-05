5468369f2a5d4b3a9fb53b21fe7c76c8a5a0dfc2
Alex Makarov <makarovav@answer-42.ru>
2026-08-05 14:40:03 +0700

    update worker

--- internal/worker/interfaces.go
+++ internal/worker/interfaces.go
@@ -1,16 +1,10 @@
-package repository
+package worker
 
 import (
 	"context"
-
-	"github.com/makaksel/MRNotifier/internal/domain"
 )
 
-type MergeRequestRepository interface {
-	UpsertMR(ctx context.Context, mr *domain.MergeRequestEvent) (bool, error)
-}
-
-type NotificationRepository interface {
-	GetNotification(ctx context.Context, id int) (*domain.Notification, error)
-	InsertNotification(ctx context.Context, projectPath string, mrIID int, n *domain.Notification) (bool, error)
+type NotificationWorker interface {
+	Start(ctx context.Context) error
+	Handle(ctx context.Context, id int) error
 }
